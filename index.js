c2540971f42b0b5d88f89ee6f7a41ee1f4c9b8ec
asraful <asrafulasraful166@gmail.com>
2025-12-22 23:36:14 +0600

    KeyConvert Added

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./reportify-firebase-adminsdk.json");
+// const serviceAccount = require("./reportify-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -1140,8 +1142,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
